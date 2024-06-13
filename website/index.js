0274949737a85a60ce03d4643381906a6b166a99
Priyansh <Priyanshyadav1012@gmail.com>
2024-06-13 13:48:20 +0530

    Implement Alert and getElementById.

---
+++ website/index.js
@@ -0,0 +1,14 @@
+console.log(`hello`);
+
+window.alert("Learning JavaScript?")
+
+//this is a Comment.
+
+/* this 
+ is  
+ a
+ multiline
+ comment */
+
+ document.getElementById('myH1').textContent = "Hello";
+ document.getElementById('myP1').textContent = "World";
