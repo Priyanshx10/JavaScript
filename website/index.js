e45307a8f9bdc48494a80f2f527a316fd4b502da
Priyansh <Priyanshyadav1012@gmail.com>
2024-06-13 13:59:50 +0530

    Implementation of Variable and use of Backticks.

--- website/index.js
+++ website/index.js
@@ -12,3 +12,27 @@ window.alert("Learning JavaScript?")
 
  document.getElementById('myH1').textContent = "Hello";
  document.getElementById('myP1').textContent ;
+
+ 
+/* 
+Variable : 
+ A Container that stores 
+ value Behaves as if it were the value it contain.
+
+ 1.Declaration 
+    let x;
+
+ 2.Assignment
+    x= 10 ;
+*/
+
+//Number
+let age = 23;
+console.log(age)
+
+//Float
+let iphonePrice = "999.99";
+console.log(iphonePrice)
+
+//Backticks implementation
+console.log(`you are ${age} years old.`)
